176d5634821ec3eb7fb8a31b00240135fc4bd255
Seokwoo Park <dev.seokwoopark@gmail.com>
2023-02-19 16:21:45 +0100

    [Add] RN-Navigator

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { TailwindProvider } from "tailwindcss-react-native";
+import HomeScreen from "./screens/HomeScreen";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <TailwindProvider>
-      <SafeAreaView className="flex-1 justify-center items-center">
-        <Text>Open up App.js to start working on your aeddpp!</Text>
-        <StatusBar style="auto" />
-      </SafeAreaView>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </TailwindProvider>
   );
 }
