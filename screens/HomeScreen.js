725c5dfa4e5d7d4be5d8e7bcac05b73fb7792706
Seokwoo Park <dev.seokwoopark@gmail.com>
2023-02-22 01:24:08 +0100

    [Add] Home page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,6 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, Image } from "react-native";
 import React from "react";
+import { HeroImage } from "../assets";
 
 const HomeScreen = () => {
   return (
@@ -9,6 +10,28 @@ const HomeScreen = () => {
           <Text className="text-[#00BCC9] text-3xl font-semibold">Go</Text>
         </View>
         <Text className="text-[#2A2B4B] text-3xl font-semibold">Travel</Text>
+      </View>
+
+      <View className="px-6 mt-8 space-y-3">
+        <Text className="text-[#3c6072] text-3xl font-semibold">
+          Enjoy the trip with
+        </Text>
+        <Text className="text-[#00bcc9] text-4xl font-bold">Good Moments</Text>
+
+        <Text className="text-[#3c6072] text-base">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </Text>
+      </View>
+
+      <View className="absolute w-[400px] h-[400px] bg-[#00bcc9] rounded-full bottom-36 -right-36" />
+      <View className="absolute w-[400px] h-[400px] bg-[#e99265] rounded-full -bottom-28 -left-36" />
+
+      <View className="flex-1 relative items-center justify-center">
+        <Image
+          source={HeroImage}
+          className="w-full h-full object-cover -bottom-10"
+        />
       </View>
     </SafeAreaView>
   );
